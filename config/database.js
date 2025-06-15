92fdee2bd11a68a673458577ec0501c15e620d3a
Garay-Ana <gabygaray1414@gmail.com>
2025-06-15 12:30:54 -0500

    Versión1

--- config/database.js
+++ config/database.js
@@ -1,14 +1,15 @@
 const { Sequelize } = require('sequelize');
 
+// Configuración de conexión a PostgreSQL en Render
 const sequelize = new Sequelize(
-  'recetaya_db',                  // Nombre de la base de datos
-  'recetaya_db_user',             // Usuario
+  'recetaya_db', // Nombre de la base de datos
+  'recetaya_db_user', // Usuario
   'FeCee4VqIfS94pF5UZWTTzreESW5eZch', // Contraseña
   {
-    host: 'dpg-d17g2gh5pdvs738bj1vg-a', // Host
-    dialect: 'postgres',               // Tipo de base de datos
-    port: 5432,                        // Puerto estándar de PostgreSQL
-    logging: false,                    // Desactiva logs SQL
+    host: 'dpg-d17g2gh5pdvs738bj1vg-a.oregon-postgres.render.com', // ✅ Host externo
+    dialect: 'postgres',
+    port: 5432,
+    logging: false, // Puedes cambiar a true si quieres ver las consultas SQL
   }
 );
 
